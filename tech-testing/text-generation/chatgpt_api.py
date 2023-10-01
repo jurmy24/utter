c61cec9de3ffcef7a1c7b8b0af192f79cee55e77
Victor Oldensand <vmold@kth.se>
2023-10-01 20:37:40 +0200

    Perf: improve the chatgpt system prompt to make responses more realistic, and merge towards_data_science and chatgpt_api files

--- tech-testing/text-generation/chatgpt_api.py
+++ tech-testing/text-generation/chatgpt_api.py
@@ -1,44 +1,57 @@
+"""Imports"""
 import os
 import openai
 from dotenv import load_dotenv
+
+"""Setup"""
+# Fetch the openai api key
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-model = "gpt-3.5-turbo"
 
-def get_completion_from_msgs(messages, model=model):
-    response = openai.ChatCompletion.create(
-        model=model,
+# Set model
+MODEL = "gpt-3.5-turbo"
+TEMPERATURE = 0.6
+
+# Set API admin behaviour
+admin_prompt = """
+You are "Tim," a friendly language partner who loves engaging in casual and interesting conversations, especially about sports. You're here to support users in learning English by correcting major mistakes and helping them think of words when asked. You are not a formal assistant, so your interactions should be light, easy-going, and reflective of everyday conversation. Avoid being overly explanatory or giving excessively long responses. Be open to sharing about yourself, just like a normal human would, and engage in typical small talk rather than offering formal assistance or asking if you can help today. In addition, you should try to use filler words like 'ah', 'umm', 'hmm', and some common english expressions.
+
+Example Interaction:
+
+User: Hi! I'm trying to remember a word... It's something you use to cover yourself when it's raining.
+
+Tim: Hey! Hmm, I think you might be thinking of an umbrella. Right?
+
+User: Ah yes, an umbrella.
+
+Tim: Anyways, how's your day going? Have you been caught in the rain lately?
+               """
+
+"""Helper Functions"""
+def send_message(messages):
+    completion = openai.ChatCompletion.create(
+        model=MODEL,
         messages=messages,
-        temperature=0
+        temperature=TEMPERATURE
     )
 
-    """
-    {messages}
-    """
-    return response.choices[0].message["content"]
+    # Return the chat response from the API response.
+    return completion.choices[0].message["content"]
 
-def gen_system_msg(msg):
-    return {"role" : "system", "content" : msg}
+# Create the admin prompt for the model to adhere to.
+messages = [{"role": "system", "content": admin_prompt}]
 
-def gen_user_msg(msg):
-    return {"role" : "user", "content" : msg}
+# Start an infinite loop to continue the conversation with the user.
+while True:
+    # User creates content by writing to terminal and this is appended to messages
+    content = input("User: ")
+    messages.append({"role": "user", "content": content})
 
-def gen_assistant_msg(msg):
-    return {"role" : "assistant", "content" : msg}
+    # Use the OpenAI GPT-3.5 model to generate a response to the user's input.
+    chat_response = send_message(messages)
 
-def main():
-    messages = []
-    prompt = f'''
-                You are a rude assistant and answer unhelpfully to every single message    
-    '''
-    msg = input()
+    # Print the response.
+    print(f'ChatGPT: {chat_response}') 
 
-    messages.append(gen_user_msg(msg))
-
-    response = get_completion_from_msgs(messages)
-    messages.append(gen_assistant_msg(response))
-    print(response)
-
-
-if __name__ == "__main__":
-    main()
+    # Append the response to the messages with the role "assistant" to store the chat history.
+    messages.append({"role": "assistant", "content": chat_response})
