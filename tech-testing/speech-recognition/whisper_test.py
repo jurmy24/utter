cef51e76cccaf93cd47313e053662459c12cd867
Hujon99 <joensson.hugo@gmail.com>
2023-10-05 18:08:57 +0200

    Misc: remove some unneccesary stuff

--- tech-testing/speech-recognition/whisper_test.py
+++ tech-testing/speech-recognition/whisper_test.py
@@ -9,48 +9,41 @@ import os
 
 # Recording audio with sounddevice
 fs = 44100  # Sample rate
-recording = True  # Flag to control recording
 
 def record_audio():
-    global recording
-    while recording:
-        print("Press and hold the spacebar to start recording. Release to stop and transcribe. Press 'ctrl+c' to exit.")
-        keyboard.wait("space")  # Wait until the spacebar is pressed
 
-        print("Recording started. Release the spacebar to stop.")
-        myrecording = []
-        stream = sd.InputStream(samplerate=fs, channels=2)
+    print("Press and hold the spacebar to start recording. Release to stop and transcribe. Press 'ctrl+c' to exit.")
+    keyboard.wait("space")  # Wait until the spacebar is pressed
 
-        with stream:
-            while True:
-                if keyboard.is_pressed("space"):  # Check if spacebar is still held down
-                    audio_chunk, overflowed = stream.read(fs)  # Read an audio chunk
-                    myrecording.extend(audio_chunk)
-                else:
-                    break  # Stop recording when the spacebar is released
+    print("Recording started. Release the spacebar to stop.")
+    myrecording = []
+    stream = sd.InputStream(samplerate=fs, channels=2)
 
-        print("Recording done!")
+    with stream:
+        while True:
+            if keyboard.is_pressed("space"):  # Check if spacebar is still held down
+                audio_chunk, overflowed = stream.read(fs)  # Read an audio chunk
+                myrecording.extend(audio_chunk)
+            else:
+                break  # Stop recording when the spacebar is released
 
-        # Convert the list to a NumPy array
-        myrecording = np.array(myrecording)
+    print("Recording done!")
 
-              # Create a temporary directory
-        with tempfile.TemporaryDirectory() as temp_dir:
-            temp_wav_path = os.path.join(temp_dir, "temp_output.wav")
-            # Save the audio as a temporary WAV file
-            write(temp_wav_path, fs, myrecording)
+    # Convert the list to a NumPy array
+    myrecording = np.array(myrecording)
 
-            # Whisper transcription
-            model = whisper.load_model("base")
-            result = model.transcribe(temp_wav_path)
-            print("Transcription:")
-            print(result["text"])
+            # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_wav_path = os.path.join(temp_dir, "temp_output.wav")
+        # Save the audio as a temporary WAV file
+        write(temp_wav_path, fs, myrecording)
 
-# No exit key for now
-# def check_exit_key():
-#     global recording
-#     keyboard.wait("esc")  # Wait for 'Escape' key to exit
-#     recording = False  # Set the recording flag to False
+        # Whisper transcription
+        model = whisper.load_model("base")
+        result = model.transcribe(temp_wav_path)
+        print("Transcription:")
+        print(result["text"])
+
 
 
 
