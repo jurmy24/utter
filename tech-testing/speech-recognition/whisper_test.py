19b4c5e1cee2d30ef435344270103ff3f554006a
Hujon99 <joensson.hugo@gmail.com>
2023-10-05 16:32:20 +0200

    Feat: add push-to-talk feature and continous running program

--- tech-testing/speech-recognition/whisper_test.py
+++ tech-testing/speech-recognition/whisper_test.py
@@ -1,24 +1,53 @@
 import whisper
 import sounddevice as sd
 from scipy.io.wavfile import write
-import time
+import keyboard
+import numpy as np
 
 
 # Recording audio with sounddevice
 fs = 44100  # Sample rate
-seconds = 4  # Duration of recording
+recording = True  # Flag to control recording
 
-print("Talk in 3 seconds!")
-time.sleep(2)
-print("Talk now!")
-time.sleep(1)
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-print("Recording done!")
-write('output.mp3', fs, myrecording)  # Save as MP3 file 
+def record_audio():
+    global recording
+    while recording:
+        print("Press and hold the spacebar to start recording. Release to stop and transcribe. Press 'Escape' to exit.")
+        keyboard.wait("space")  # Wait until the spacebar is pressed
+
+        print("Recording started. Release the spacebar to stop.")
+        myrecording = []
+        stream = sd.InputStream(samplerate=fs, channels=2)
+
+        with stream:
+            while True:
+                if keyboard.is_pressed("space"):  # Check if spacebar is still held down
+                    audio_chunk, overflowed = stream.read(fs)  # Read an audio chunk
+                    myrecording.extend(audio_chunk)
+                else:
+                    break  # Stop recording when the spacebar is released
+
+        print("Recording done!")
+
+        # Convert the list to a NumPy array
+        myrecording = np.array(myrecording)
+
+        # Save the audio as a WAV file
+        write('output.wav', fs, myrecording)
+
+        # Whisper transcription
+        model = whisper.load_model("base")
+        result = model.transcribe("output.wav")
+        print("Transcription:")
+        print(result["text"])
+
+# No exit key for now
+# def check_exit_key():
+#     global recording
+#     keyboard.wait("esc")  # Wait for 'Escape' key to exit
+#     recording = False  # Set the recording flag to False
 
 
-# Whisper transcription
-model = whisper.load_model("base")
-result = model.transcribe("output.mp3")
-print(result["text"])
+
+if __name__ == "__main__":
+    record_audio()
