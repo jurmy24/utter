a63765729ed4e2b820b8c2d242a3d28b372755eb
Hujon99 <joensson.hugo@gmail.com>
2023-10-06 11:51:19 +0200

    Feat: use whisper as transcriber

--- tech-testing/tech-integration/main.py
+++ tech-testing/tech-integration/main.py
@@ -3,6 +3,6 @@ from modules import chatbot, asr, synthesis
 if __name__ == "__main__":
 
     while True:
-        user_msg = asr.record_audio()
+        user_msg = asr.get_user_audio_whisper()
         assistant_msg = chatbot.chat(user_msg)
         synthesis.synthesize_speech(assistant_msg)
