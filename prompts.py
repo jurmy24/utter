bd2758e8ff0cb99b3b45d75b2c5a0665a1e50ee8
Victor Oldensand <victormag24@gmail.com>
2024-08-06 18:56:32 +0200

    Feat: create empty(ish) prompts file to fill later

---
+++ prompts.py
@@ -0,0 +1,1 @@
+duolingo = "Write an exercise that uses the word {word} in {language}.\n\nRules:\n1. The exercise must have two answer options.\n2. The exercise must be fewer than 75 characters.\n3. The exercise must be written in {level} CEFR level {language}.\n4. The exercise must contain THE PRETERITE TENSE and THE IMPERFECT TENSE.\n\nGo!\n"
